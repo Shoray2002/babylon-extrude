24676a96fb16b7277d218c7692f96a6965e76930
shoray2002 <shoryasinghall@gmail.com>
2023-06-21 23:33:54 +0530

    init

---
+++ main.js
@@ -0,0 +1,41 @@
+import * as BABYLON from "babylonjs";
+const canvas = document.getElementById("canvas");
+const engine = new BABYLON.Engine(canvas, true);
+const createScene = function () {
+  const scene = new BABYLON.Scene(engine);
+  const camera = new BABYLON.FreeCamera(
+    "camera1",
+    new BABYLON.Vector3(0, 5, -10),
+    scene
+  );
+  camera.setTarget(BABYLON.Vector3.Zero());
+  camera.attachControl(canvas, true);
+  const light = new BABYLON.HemisphericLight(
+    "light",
+    new BABYLON.Vector3(0, 1, 0),
+    scene
+  );
+  light.intensity = 0.7;
+  const sphere = BABYLON.MeshBuilder.CreateSphere(
+    "sphere",
+    { diameter: 2, segments: 32 },
+    scene
+  );
+  sphere.position.y = 1;
+
+  const ground = BABYLON.MeshBuilder.CreateGround(
+    "ground",
+    { width: 6, height: 6 },
+    scene
+  );
+  return scene;
+};
+const scene = createScene();
+
+engine.runRenderLoop(function () {
+  scene.render();
+});
+
+window.addEventListener("resize", function () {
+  engine.resize();
+});
